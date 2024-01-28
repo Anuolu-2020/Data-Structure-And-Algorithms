0b6f5b3931fbcc7ce76a4ca547f4c39652cf0e9d
Anuolu-2020 <Anuolu1645@gmail.com>
2024-01-28 18:26:47 +0100

    chore: minor changes

--- src/Algorithms/bubble-sort.ts
+++ src/Algorithms/bubble-sort.ts
@@ -15,4 +15,4 @@ function sort(numbers: number[]) {
 
 console.log(sort([2, 1, 6, 5, 9, 7, 4, 3]));
 console.log(sort([-8, 1, 3, 5, -3, 7, 2, 4]));
-console.log(sort([10,2,-1,3,-22]));
+console.log(sort([10, 2, -1, 3, -22]));
